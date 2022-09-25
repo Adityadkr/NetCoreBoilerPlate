4e453b7ca00210a4127357b87c73e70167b458c6
Adiya Deokar <adityadkr@gmail.com>
2022-09-26 00:10:47 +0530

    Datatable helper

--- WebApp/Controllers/DemoController.cs
+++ WebApp/Controllers/DemoController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DbEntities.Models.MongoModels;
+using DbServices.IRepositories;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +10,21 @@ namespace WebApp.Controllers
 {
     public class DemoController : Controller
     {
+        private readonly IDemo _demo;
+        public DemoController(IDemo demo)
+        {
+            _demo = demo;
+        }
         public IActionResult Index()
         {
-            throw new Exception("system");
-            //return View();
+
+            //throw new Exception("system");
+            return View();
+        }
+        public List<Alien> GetAliens() {
+
+            var data = _demo.GetAliens();
+            return data;
         }
     }
 }
---
+++ WebApp/wwwroot/js/HelperJS/GridHelper.js
@@ -0,0 +1,72 @@
+﻿
+function CreateCommonGridWithData(table, data, columns) {
+
+    $(table).DataTable({
+        dom: 'Bfrtip',
+        "autoWidth": false,
+        "processing": true,
+        "filter": true, // this is for disable filter (search box)  
+        "orderMulti": false, // for disable multiple column at once  
+        "ordering": true,
+        "scrollX": true,
+        data: data,
+        columns: columns
+    });
+}
+function CreateCommonGridAjax(table, url, columns) {
+    //WHAT TO PASS IN COLUMNS ARRAY
+    //[
+    //    { "data": "id", "name": "Id" },
+    //]
+
+    $(table).DataTable({
+        dom: 'Bfrtip',
+        "autoWidth": false,
+        "processing": true,
+        "filter": true, // this is for disable filter (search box)  
+        "orderMulti": false, // for disable multiple column at once  
+        "ordering": true,
+        "scrollX": true,
+        buttons: [
+            'colvis',
+            'excel',
+            'print'
+        ],
+        "ajax": {
+            "type": "GET",
+            "url": url,
+            "dataSrc": function (json) {
+                return json
+            }
+        },
+
+        columns: columns
+    });
+}
+
+function CreateCommonGridServerSide(table, url, columns) {
+    $(table).DataTable({
+        dom: 'Bfrtip',
+        "processing": true, // for show progress bar  
+        "serverSide": true, // for process server side  
+        "autoWidth": false,
+        "filter": true, // this is for disable filter (search box)  
+        "orderMulti": false, // for disable multiple column at once  
+        "ordering": true,
+        "scrollX": true,
+        buttons: [
+            'colvis',
+            'excel',
+            'print'
+        ],
+        "ajax": {
+            "type": "GET",
+            "url": url,
+            "dataSrc": function (json) {
+                return json
+            }
+        },
+
+        columns: columns
+    });
+}
